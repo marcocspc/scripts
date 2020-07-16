dfc28479938d345bdfa1c5ab8d05e4ac6a90e6e4
Marco Antonio <marcocspc@hotmail.com>
2020-07-16 00:07:38 -0300

    Update mount_sftp_host.sh

--- macos/mount_sftp_host.sh
+++ macos/mount_sftp_host.sh
@@ -8,6 +8,9 @@ VOLUME_NAME="Name to show on /Volumes"
 KEY_PATH="/path/to/keyfile" #KEY_PATH CANNOT HAVE SPACES, FOR SOME WEIRD REASON
 IP_OR_NAME="ip or name of the server"
 REMOTE_ADDRESS="/path/on/server/to/mount"
+PORT="22"
 
-mkdir "/Volumes/$VOLUME_NAME"
-sshfs -o reconnect -o volname="$VOLUME_NAME" -o IdentityFile="$KEY_PATH" "$USER@$IP_OR_NAME":"$REMOTE_ADDRESS" "/Volumes/$VOLUME_NAME"
+for i in $(seq 1 $END); do
+    mkdir "/Volumes/$VOLUME_NAME$i" || echo "" 
+    sshfs -o reconnect -o volname="$VOLUME_NAME" "$USER@$IP_OR_NAME":"$REMOTE_ADDRESS" "/Volumes/$VOLUME_NAME$i" -p $PORT || echo "Tried to mound /Volumes/$VOLUME_NAME$i, didn't work, trying next number."
+done
